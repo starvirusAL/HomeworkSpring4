973b45af5ede57fe0d881fc3930959d328fb6fd2
starvirusAL <alexoid1990@gmail.com>
2023-08-17 19:39:14 +0300

    encoded

---
+++ src/main/java/app/Security/MySecurityConfig.java
@@ -0,0 +1,21 @@
+package app.Security;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+import java.util.Arrays;
+
+@Configuration
+@EnableWebSecurity
+public class MySecurityConfig extends WebSecurityConfigurerAdapter {
+
+
+
+
+}
